259a7fd8ee66c03ac9b58b54b1d13ba6375737a4
inlguy <5023364+inlguy@users.noreply.github.com>
2020-04-02 17:12:04 -0600

    deleted CPAAdmin

--- controller/CPAAdmin/CPAAdmin/AdminAPI/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    return render(request, 'AdminAPI/index.html')
