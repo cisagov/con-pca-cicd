205601a8155d6ac4a02971f5a7b257bf3c530e4c
Kuipers, Jason Eric <jason.kuipers@inl.gov>
2020-04-02 15:15:48 -0600

    styling changes and theming

--- controller/AdminUI/src/app/components/layout/layout-main/layout-main.component.ts
+++ controller/AdminUI/src/app/components/layout/layout-main/layout-main.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout-main',
   templateUrl: './layout-main.component.html',
-  styleUrls: [ './layout-main.component.scss' ]
+  styleUrls: [ './layout-main.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class LayoutMainComponent implements OnInit {
 
   constructor() {
---
+++ controller/AdminUI/src/app/components/search-panel/search-panel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-panel',
+  templateUrl: './search-panel.component.html'
+})
+export class SearchPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
