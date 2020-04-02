2e859e223592c46c031171278a3b5aa1b8dad6b8
Jason Kuipers <kuipers.jason@gmail.com>
2020-04-02 11:31:31 -0600

    fix all things

--- controller/CPAAdmin/CPAAdmin/AdminUI/src/app/app-routing.module.ts
+++ controller/CPAAdmin/CPAAdmin/AdminUI/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutBlankComponent } from './components/layout/layout-blank/layout-blank.component';
+import { LayoutMainComponent } from './components/layout/layout-main/layout-main.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'dashboard', component: LayoutMainComponent, 
+    children: [
+      { path: '', component: DashboardComponent}
+    ]
+  },
+  {
+    path:'', component: LayoutMainComponent,
+    children: [
+      { path: '', component: DashboardComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
