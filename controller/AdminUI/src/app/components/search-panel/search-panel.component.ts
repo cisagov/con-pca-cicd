0b207566097a4e4df0fc6297747a1366bd6111b1
Randy Woods <randy1@radiorandy.com>
2020-04-02 13:34:13 -0600

    Create search panel

---
+++ controller/AdminUI/src/app/components/search-panel/search-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-panel',
+  templateUrl: './search-panel.component.html',
+  styleUrls: ['./search-panel.component.sass']
+})
+export class SearchPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
