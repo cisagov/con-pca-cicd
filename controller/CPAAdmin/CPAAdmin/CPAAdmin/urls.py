7a974a263f6ebc9353317040ba0610be1e8ae634
Barry K. Hansen <89001000073007@FEDIDCARD.GOV>
2020-04-02 07:44:20 -0600

    getting the AdminUI up and running

--- controller/CPAAdmin/CPAAdmin/CPAAdmin/urls.py
+++ controller/CPAAdmin/CPAAdmin/CPAAdmin/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('AdminAPI/', include('AdminAPI.urls')),
+    path('', include('AdminAPI.urls')),
     path('admin/', admin.site.urls),
 ]
