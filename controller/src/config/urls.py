53d722fdf28a7ad37d0199eae13234bdc25db8ad
mostafa <hackbook@hackers-MBP.attlocal.net>
2020-04-02 20:13:26 -0500

    feature: gophish integration

--- controller/src/config/urls.py
+++ controller/src/config/urls.py
@@ -7,6 +7,7 @@ from reports.views import HomePageView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reports/', include('reports.urls')),
+    path('subscriptions/', include('subscriptions.urls')),
     path('tasks/', include('tasks.urls')),
-    path("", HomePageView.as_view()),
+    path("", HomePageView.as_view(), name="home"),
 ]
