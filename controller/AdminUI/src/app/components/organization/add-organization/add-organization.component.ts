8546c74767375c05384c2bcec4e9bf7a8cdcf173
Jason Kuipers <kuipers.jason@gmail.com>
2020-04-06 15:25:32 -0600

    add component code

---
+++ controller/AdminUI/src/app/components/organization/add-organization/add-organization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-organization',
+  templateUrl: './add-organization.component.html',
+  styleUrls: ['./add-organization.component.scss']
+})
+export class AddOrganizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+}
