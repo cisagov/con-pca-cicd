89255f34272757995e435cc1501913321a58b1a5
Jason Kuipers <kuipers.jason@gmail.com>
2020-04-07 13:51:18 -0600

    initial push

--- controller/AdminUI/src/app/components/service-subscription/service-subscription.component.ts
+++ controller/AdminUI/src/app/components/service-subscription/service-subscription.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { AddOrganizationComponent} from '../../components/organization/add-organization/add-organization.component';
 
 @Component({
   selector: 'app-service-subscription',
   templateUrl: './service-subscription.component.html', 
-  styleUrls: ['./service-subscription.component.scss']
+  styleUrls: ['./service-subscription.component.scss'],
+  directives: [ AddOrganizationComponent ] 
 })
 export class ServiceSubscriptionComponent implements OnInit {
    
