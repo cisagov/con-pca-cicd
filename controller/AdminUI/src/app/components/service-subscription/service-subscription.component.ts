cebc990697b6e8578d72723ae59eece97988bb60
Randy Woods <randy1@radiorandy.com>
2020-04-07 15:12:10 -0600

    Merged Jason's code into my branch

--- controller/AdminUI/src/app/components/service-subscription/service-subscription.component.ts
+++ controller/AdminUI/src/app/components/service-subscription/service-subscription.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { AddOrganizationComponent} from '../../components/organization/add-organization/add-organization.component';
 
 @Component({
   selector: 'app-service-subscription',
-  directives: [ AddOrganizationComponent ] 
   styleUrls: ['./service-subscription.component.scss'],
-  templateUrl: './service-subscription.component.html', 
+  templateUrl: './service-subscription.component.html'
 })
 export class ServiceSubscriptionComponent implements OnInit {
 
