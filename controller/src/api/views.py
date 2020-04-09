f7fcbb025cd4f139e481b04f6103cbe04b7970e6
pmercado23 <pmercado@ucsc.edu>
2020-04-09 16:46:06 -0400

    updating get for subscriptions

--- controller/src/api/views.py
+++ controller/src/api/views.py
@@ -1,32 +1,41 @@
-import json
-from datetime import datetime, date
+"""
+This is the main views for api.
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
+This handles api views
+"""
+# Standard Python Libraries
+import asyncio
+
+# Third-Party Libraries
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from . import serializers
-from .utils import Subscription
+from .models import SubscriptionModel, validate_subscription
+from .utils import db_service
 
 
 class SubscriptionsView(APIView):
+    """
+    This is the SubscriptionsView APIView.
+
+    This handles the API for the Substriptions.
+    """
+
     def get(self, request):
+        """Get method."""
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        service = db_service("subscriptions", SubscriptionModel, validate_subscription)
+        subscription_list = loop.run_until_complete(service.filter_list(parameters={}))
 
-        subscriptions = Subscription(
-            name="SC-1031.Matt-Daemon.1.1",
-            status="Waiting on SRF",
-            primary_contact="Matt Daemon",
-            customer="Some Company.2com",
-            last_action=date.today(),
-            active=True
-        )
-
-        serializer = serializers.SubscriptionSerializer(subscriptions)
-
-        return Response(serializer.data)
+        return Response(subscription_list)
 
     def post(self, request, format=None):
+        """Post method."""
         serializer = serializers.SubscriptionSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
