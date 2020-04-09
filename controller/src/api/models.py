004e9e0d7ed90fa79e4cd3a259d1136f80265eb0
pmercado23 <pmercado@ucsc.edu>
2020-04-09 17:37:57 -0400

    basic sub data agrogation

--- controller/src/api/models.py
+++ controller/src/api/models.py
@@ -1,24 +1,23 @@
 """Models."""
 # Third-Party Libraries
 from database.repository.models import Model
-from database.repository.types import DateTimeType, StringType, UUIDType
+from database.repository.types import BooleanType, DateTimeType, StringType, UUIDType
 
 
 class SubscriptionModel(Model):
     """
-    This is an example Model.
+    This is the Subscription Model.
 
-    This shows basic types that we will use on each model.
+    This controls all data needed in saving the model.
     """
 
     subscription_uuid = UUIDType()
     name = StringType()
-    enum_type = StringType(required=True, choices=("initial", "post", "pre", "final"))
-    record_tstamp = DateTimeType(required=True)
-    method_of_record_creation = StringType()
-    last_updated_by = StringType(required=False, max_length=255)
-    lub_timestamp = DateTimeType(required=False)
-    method_of_lu = StringType(required=False)
+    status = StringType()
+    primary_contact = StringType()
+    customer = StringType()
+    last_action = DateTimeType(required=False)
+    active = BooleanType()
 
 
 def validate_subscription(data_object):
