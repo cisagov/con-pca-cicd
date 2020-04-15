a4c97d55f2631d8ba9fa08890887b2ddafaba529
inlguy <5023364+inlguy@users.noreply.github.com>
2020-04-15 13:53:50 -0600

    Moving the angular in

---
+++ client/AdminUI/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SubscriptionsComponent } from './components/subscriptions/subscriptions.component';
+import { LayoutBlankComponent } from './components/layout/layout-blank/layout-blank.component';
+import { LayoutMainComponent } from './components/layout/layout-main/layout-main.component';
+import { SearchPanelComponent } from './components/search-panel/search-panel.component';
+import { ServiceSubscriptionComponent } from './components/service-subscription/service-subscription.component';
+import { CreateSubscriptionComponent } from './components/subscriptions/create-subscription/create-subscription.component';
+import { AutosizeModule } from 'node_modules/ngx-autosize';
+import { AddOrganizationComponent } from './components/organization/add-organization/add-organization.component';
+import { SubscriptionService } from './services/subscription.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    SubscriptionsComponent, 
+    LayoutBlankComponent, 
+    LayoutMainComponent, 
+    SearchPanelComponent, 
+    ServiceSubscriptionComponent,
+    AddOrganizationComponent,
+    CreateSubscriptionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MaterialModule, 
+    FormsModule, 
+    ReactiveFormsModule,
+    AutosizeModule,
+    HttpClientModule
+  ],
+  providers: [
+    SubscriptionService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/AdminUI/src/app/components/service-subscription/service-subscription.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-service-subscription',
+  styleUrls: ['./service-subscription.component.scss'],
+  templateUrl: './service-subscription.component.html',
+})
+export class ServiceSubscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
