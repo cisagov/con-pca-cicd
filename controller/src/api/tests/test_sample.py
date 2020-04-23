359cf7ab9e8b55b6f1f4fa6b5df7adbbcb9353cb
Dylan Johnson <dylanj1752@gmail.com>
2020-04-23 16:11:55 -0600

    pre-commit learning

---
+++ controller/src/api/tests/test_sample.py
@@ -0,0 +1,11 @@
+"""Example Test structure."""
+
+
+def example_func(x):
+    """Example function."""
+    return x + 1
+
+
+def test_func():
+    """Example test function. Any function begining with 'test_' will be included in the pytest call."""
+    # assert example_func(3) == 4
