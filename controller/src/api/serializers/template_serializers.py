2913765880a055e50c6a5ba4389a75c13131dd26
pmercado23 <pmercado@ucsc.edu>
2020-04-28 14:33:33 -0400

    updating template serizlizers

--- controller/src/api/serializers/template_serializers.py
+++ controller/src/api/serializers/template_serializers.py
@@ -8,6 +8,54 @@ serializing data coming from the db into a request responce.
 from rest_framework import serializers
 
 
+class TemplateAppearanceSerializer(serializers.Serializer):
+    """
+    This is the Template Serializer.
+
+    This holds values for template Appearance Score.
+    """
+
+    grammar = serializers.IntegerField()
+    link_domain = serializers.IntegerField()
+    logo_graphics = serializers.IntegerField()
+
+
+class TemplateSenderSerializer(serializers.Serializer):
+    """
+    This is the Template Sender Serializer.
+
+    This holds values for template Sender Score.
+    """
+
+    external = serializers.IntegerField()
+    internal = serializers.IntegerField()
+    authoritative = serializers.IntegerField()
+
+
+class TemplateRelevancySerializer(serializers.Serializer):
+    """
+    This is the Template Relevancy Serializer.
+
+    This holds values for template Relevancy Score.
+    """
+
+    organization = serializers.IntegerField()
+    public_news = serializers.IntegerField()
+
+
+class TemplateBehaviorSerializer(serializers.Serializer):
+    """
+    This is the Template Behavior Model.
+
+    This holds values for template Behavior Score.
+    """
+
+    fear = serializers.IntegerField()
+    duty_obligation = serializers.IntegerField()
+    curiosity = serializers.IntegerField()
+    greed = serializers.IntegerField()
+
+
 class TemplateGetSerializer(serializers.Serializer):
     """
     This is the Template GET Serializer.
@@ -16,29 +64,22 @@ class TemplateGetSerializer(serializers.Serializer):
     """
 
     template_uuid = serializers.UUIDField()
-    name = serializers.CharField(max_length=200)
-    system_path = serializers.CharField(max_length=200)
+    name = serializers.CharField()
     deception_score = serializers.IntegerField()
     descriptive_words = serializers.DictField()
-    description = serializers.CharField(max_length=200)
+    description = serializers.CharField()
     display_link = serializers.CharField(max_length=200)
-    from_address = serializers.CharField(max_length=200)
+    from_address = serializers.EmailField()
     retired = serializers.BooleanField()
     subject = serializers.CharField(max_length=200)
-    text = serializers.CharField(max_length=200)
+    text = serializers.CharField()
     topic_list = serializers.ListField()
-    grammer = serializers.IntegerField()
-    link_domain = serializers.IntegerField()
-    logo_graphics = serializers.IntegerField()
-    external = serializers.IntegerField()
-    internal = serializers.IntegerField()
-    authoritative = serializers.IntegerField()
-    organization = serializers.IntegerField()
-    public_news = serializers.IntegerField()
-    fear = serializers.IntegerField()
-    duty_obligation = serializers.IntegerField()
-    curiosity = serializers.IntegerField()
-    greed = serializers.IntegerField()
+    # Score data
+    appearance = TemplateAppearanceSerializer()
+    sender = TemplateSenderSerializer()
+    relevancy = TemplateRelevancySerializer()
+    behavior = TemplateBehaviorSerializer()
+    complexity = serializers.IntegerField()
     # db_tracting data added below
     created_by = serializers.CharField(max_length=200)
     cb_timestamp = serializers.DateTimeField()
@@ -53,29 +94,22 @@ class TemplatePostSerializer(serializers.Serializer):
     This is a formats the data coming out of the Db.
     """
 
-    name = serializers.CharField(max_length=200)
-    system_path = serializers.CharField(max_length=200)
+    name = serializers.CharField()
     deception_score = serializers.IntegerField()
     descriptive_words = serializers.DictField()
-    description = serializers.CharField(max_length=200)
+    description = serializers.CharField()
     display_link = serializers.CharField(max_length=200)
-    from_address = serializers.CharField(max_length=200)
+    from_address = serializers.EmailField()
     retired = serializers.BooleanField()
     subject = serializers.CharField(max_length=200)
-    text = serializers.CharField(max_length=200)
+    text = serializers.CharField()
     topic_list = serializers.ListField()
-    grammer = serializers.IntegerField()
-    link_domain = serializers.IntegerField()
-    logo_graphics = serializers.IntegerField()
-    external = serializers.IntegerField()
-    internal = serializers.IntegerField()
-    authoritative = serializers.IntegerField()
-    organization = serializers.IntegerField()
-    public_news = serializers.IntegerField()
-    fear = serializers.IntegerField()
-    duty_obligation = serializers.IntegerField()
-    curiosity = serializers.IntegerField()
-    greed = serializers.IntegerField()
+    # Score data
+    appearance = TemplateAppearanceSerializer()
+    sender = TemplateSenderSerializer()
+    relevancy = TemplateRelevancySerializer()
+    behavior = TemplateBehaviorSerializer()
+    complexity = serializers.IntegerField()
 
 
 class TemplatePostResponseSerializer(serializers.Serializer):
