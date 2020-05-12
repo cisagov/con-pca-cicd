067a6d1fb880d876312a75e7ac36f6f9b29450a7
pmercado23 <pmercado@ucsc.edu>
2020-05-12 16:27:12 -0400

    updating gophish models

--- controller/src/api/models/subscription_models.py
+++ controller/src/api/models/subscription_models.py
@@ -9,6 +9,7 @@ from database.repository.types import (
     BooleanType,
     DateTimeType,
     EmailType,
+    FloatType,
     IntType,
     ListType,
     ModelType,
@@ -68,19 +69,100 @@ class SubscriptionClicksModel(Model):
     target_uuid = UUIDType()
 
 
+class GoPhishResultModel(Model):
+    """
+    This is the GoPhish Result Model.
+
+    This is a format to hold target information in the subscription model.
+    id                   : int64
+    first_name           : string
+    last_name            : string
+    position             : string
+    status               : string
+    ip                   : string
+    latitude             : float
+    longitude            : float
+    send_date            : string(datetime)
+    reported             : boolean
+    """
+
+    id = IntType()
+    first_name = StringType()
+    last_name = StringType()
+    position = StringType()
+    status = StringType()
+    ip = StringType()
+    latitude = FloatType()
+    longitude = FloatType()
+    send_date = DateTimeType()
+    reported = BooleanType()
+
+
+class GoPhishGroupModel(Model):
+    """
+    This is the SubscriptionClicks Model.
+
+    This is a format to hold target information in the subscription model.
+    id              : int64
+    name            : string
+    targets         : array(Target)
+    modified_date   : string(datetime)
+    """
+
+    id = IntType()
+    name = StringType()
+    targets = ListType(ModelType(SubscriptionTargetModel))
+    modified_date = DateTimeType()
+
+
+class GoPhishTimelineModel(Model):
+    """
+    This is the GoPhish Timeline Model.
+
+    This is a format to hold target information in the subscription model.
+    email                : string
+    time                 : string(datetime)
+    message              : string
+    details              : string(JSON)
+    """
+
+    email = StringType()
+    time = DateTimeType()
+    message = StringType()
+    details = StringType()
+
+
 class GoPhishCampaignsModel(Model):
     """
-    This is the GoPhishCampaigns Model.
+    This is the GoPhish Campaigns Model.
 
     This is a format to hold GophishCampaign information in the subscription model.
+    id                  : int64
+    name                : string
+    created_date        : string(datetime)
+    launch_date         : string(datetime)
+    send_by_date        : string(datetime)
+    completed_date      : string(datetime)
+    template            : Template
+    page                : Page
+    status              : string
+    results             : []Result
+    groups              : []Group
+    timeline            : []Event
     """
 
     campaign_id = IntType()
     name = StringType()
+    created_date = DateTimeType()
+    launch_date = DateTimeType()
+    send_by_date = DateTimeType()
+    completed_date = DateTimeType()
     email_template = StringType()
     landing_page_template = StringType()
-    start_date = DateTimeType()
-    end_date = DateTimeType()
+    status = StringType()
+    results = ListType(ModelType(GoPhishResultModel))
+    groups = ListType(ModelType(GoPhishGroupModel))
+    timeline = ListType(ModelType(GoPhishTimelineModel))
     target_email_list = ListType(ModelType(SubscriptionTargetModel))
 
 
