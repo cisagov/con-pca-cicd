bec7a0f8977406cd388fd08d7f3f972a958e5f44
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-05-16 18:13:21 -0500

    update task views

--- controller/src/tasks/tasks.py
+++ controller/src/tasks/tasks.py
@@ -3,8 +3,13 @@ import os
 from celery import Celery, shared_task
 from celery.schedules import crontab
 
+from config.celery import app
+
 
 @shared_task
-def create_task(task_type):
-    time.sleep(int(task_type) * 10)
-    return True
+def campaign_report(word):
+    """
+    Pull final campaign report
+    """
+    print(word)
+    return word
--- controller/src/tasks/views.py
+++ controller/src/tasks/views.py
@@ -1,24 +1,45 @@
-from celery.result import AsyncResult
+from celery.result import AsyncResult, current_app
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .tasks import create_task
+from config.celery import app
+from .tasks import campaign_report
 
-
-@csrf_exempt
-def run_task(request):
-    if request.POST:
-        task_type = request.POST.get("type")
-        task = create_task.delay(int(task_type))
-        return JsonResponse({"task_id": task.id}, status=202)
+inspect = app.control.inspect()
 
 
-@csrf_exempt
-def get_status(request, task_id):
-    task_result = AsyncResult(task_id)
-    result = {
-        "task_id": task_id,
-        "task_status": task_result.status,
-        "task_result": task_result.result
-    }
-    return JsonResponse(result, status=200)
+class TaskListView(APIView):
+    def get(self, request):
+        """
+        Return a list of active, scheduled, reserved
+        and registered tasks. Also specifies which celery worker
+        the task will be executed on
+        """
+        context = {
+            "active": inspect.active(),
+            "scheduled": inspect.scheduled(),
+            "reserved": inspect.reserved(),
+            "registered": inspect.registered(),
+        }
+        return Response(context)
+
+    def post(self, request):
+        data = request.data
+        word = data.get("word")
+        task = campaign_report.apply_async(args=[word], countdown=30)
+        context = {"task id": task.id, "word": word}
+        return Response(context)
+
+
+class TaskView(APIView):
+    def get(self, request, task_id):
+        task_result = AsyncResult(task_id)
+        result = {
+            "task_id": task_id,
+            "task_status": task_result.status,
+            "task_result": task_result.result,
+        }
+
+        return Response(result)
