882487fea3a5673c70f38e7390ae49f3fa0aaa82
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-05-20 15:57:03 -0500

    add initial reports view

--- controller/src/api/views/report_views.py
+++ controller/src/api/views/report_views.py
@@ -12,6 +12,18 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+# Local Libraries
+from api.models.subscription_models import SubscriptionModel, validate_subscription
+from api.serializers.subscriptions_serializers import SubscriptionGetSerializer
+from api.manager import CampaignManager
+from api.utils.db_utils import get_single
+
+
+logger = logging.getLogger(__name__)
+
+# GoPhish API Manager
+campaign_manager = CampaignManager()
+
 
 class ReportsView(APIView):
     """
@@ -19,3 +31,15 @@ class ReportsView(APIView):
 
     This handles the API a Get .
     """
+
+    def get(self, request, subscription_uuid):
+        subscription = get_single(
+            subscription_uuid, "subscription", SubscriptionModel, validate_subscription
+        )
+        campaigns = subscription.get("gophish_campaign_list")
+        summary = [
+            campaign_manager.get("summary", campaign_id=campaign.get("campaign_id"))
+            for campaign in campaigns
+        ]
+
+        return Response(summary)
