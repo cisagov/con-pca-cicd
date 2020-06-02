dbdd3ab32287975981550f2ad0207c00f1f88f1b
Marcus <mbgoec2000@gmail.com>
2020-06-02 14:12:21 -0600

    Reorganized campaign serializers tests

---
+++ controller/src/api/tests/serializers/campaign_serializers/test_campaign_group_target.py
@@ -0,0 +1,19 @@
+from api.serializers.campaign_serializers import CampaignGroupTargetSerializer
+
+def create(email, first_name, last_name, position):
+        data = {
+            "email": email,
+            "first_name": first_name,
+            "last_name": last_name,
+            "position": position,
+        }
+        serializer = CampaignGroupTargetSerializer(data=data)
+        return serializer
+
+def test_creation():
+    serializer = create(
+        "someemail@domain.com", "firstname", "lastname", "someposition"
+    )
+    assert isinstance(serializer, CampaignGroupTargetSerializer)
+    assert serializer.is_valid()
+    assert len(serializer.errors) == 0
