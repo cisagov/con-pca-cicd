b47b13917b814fbde085a9ddd87a733cffa0f620
Zachary Wilson <wilsonzachary634@gmail.com>
2020-06-11 08:13:43 -0600

    updates to linux scripts

--- check_git.sh
+++ check_git.sh
@@ -9,7 +9,8 @@ changes=$(git rev-list HEAD..origin/develop --count)
 if [ $changes -ne 0 ]; then
    echo "Need to pull"
    git pull
-   bash /app/con-pca/linux-vm-deploy.sh > /app/build_log.txt 2> /app/build_error.txt 
+   date > /app/build_log.txt
+   bash /app/con-pca/linux-vm-deploy.sh >> /app/build_log.txt 2> /app/build_error.txt 
 else
    echo "All good"
 fi
