91a424508770466de49a03ea834b45ffb7767367
Zachary Wilson <wilsonzachary634@gmail.com>
2020-06-11 07:58:09 -0600

    rename script for deploying to linux vm

--- check_git.sh
+++ check_git.sh
@@ -9,7 +9,7 @@ changes=$(git rev-list HEAD..origin/develop --count)
 if [ $changes -ne 0 ]; then
    echo "Need to pull"
    git pull
-   bash /app/con-pca/linux-vm-setup.sh > /app/build_log.txt 2> /app/build_error.txt 
+   bash /app/con-pca/linux-vm-deploy.sh > /app/build_log.txt 2> /app/build_error.txt 
 else
    echo "All good"
 fi
