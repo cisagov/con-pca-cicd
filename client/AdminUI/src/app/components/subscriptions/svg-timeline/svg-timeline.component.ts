a25add2aaa44f07f6ec732dc1126a9a400225c05
Randy Woods <randy1@radiorandy.com>
2020-06-15 15:27:38 -0600

    Refining the SVG content

--- client/AdminUI/src/app/components/subscriptions/svg-timeline/svg-timeline.component.ts
+++ client/AdminUI/src/app/components/subscriptions/svg-timeline/svg-timeline.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TimelineItem } from 'src/app/models/subscription.model';
 
 @Component({
   selector: 'app-svg-timeline',
@@ -6,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SvgTimelineComponent implements OnInit {
 
+  @Input()
+  timelineItems: TimelineItem[];
+
+  items: any[] = [];
+
+  /**
+   *
+   */
   constructor() { }
 
+  /**
+   *
+   */
   ngOnInit(): void {
+    this.xyz();
+  }
+
+  /**
+   *
+   */
+  xyz() {
+    console.log('xyz');
+    console.log(this.timelineItems);
+    let itemIndex = 0;
+    this.timelineItems.forEach(x => {
+      const item = {
+        x: itemIndex * 100
+      };
+
+      this.items.push(item);
+
+      itemIndex = itemIndex + 1;
+    });
   }
 
 }
