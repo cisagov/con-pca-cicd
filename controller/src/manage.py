759384f6443a0844d850ab40e853bf0ea64f9df9
Zachary Wilson <wilsonzachary634@gmail.com>
2020-06-16 08:41:49 -0600

    manage py changes

--- controller/src/manage.py
+++ controller/src/manage.py
@@ -2,19 +2,21 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.conf import settings
+
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     try:
         from django.core.management import execute_from_command_line
-        # from django.conf import settings
+
+        from django.conf import settings
 
         if settings.DEBUG:
-           if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
-               import ptvsd
-               ptvsd.enable_attach(address = ('0.0.0.0', 5678))
-               print("NOT Attached remote debugger")
+            if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+                import ptvsd
+
+                ptvsd.enable_attach(address=("0.0.0.0", 5678))
+                print("NOT Attached remote debugger")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -24,5 +26,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
