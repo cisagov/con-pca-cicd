2a23ec1b4f2b015127e1b7781021436405079055
pmercado23 <pmercado@ucsc.edu>
2020-06-25 11:31:15 -0400

    creating target history model and serializers

--- controller/src/api/serializers/template_serializers.py
+++ controller/src/api/serializers/template_serializers.py
@@ -322,3 +322,66 @@ class TagQuerySerializer(serializers.Serializer):
     cb_timestamp = serializers.DateTimeField(required=False)
     last_updated_by = serializers.CharField(required=False)
     lub_timestamp = serializers.DateTimeField(required=False)
+
+
+class TemplateStatusSerializer(serializers.Serializer):
+    """
+    Serializes Template Status.
+
+    This handles nested data for Tempalte history
+    """
+
+    template_uuid = serializers.UUIDField()
+    sent_timestamp = serializers.DateTimeField(required=False)
+
+
+class TargetHistoryGetSerializer(serializers.Serializer):
+    """
+    Serializes Template History.
+
+    This handles the history of a targets tempaltes.
+    """
+
+    # created by mongodb
+    target_uuid = serializers.UUIDField()
+    # User Defined
+    email = serializers.EmailField(required=True)
+    history_list = TemplateStatusSerializer(many=True)
+    # db_tracting data added below
+    created_by = serializers.CharField(required=False)
+    cb_timestamp = serializers.DateTimeField(required=False)
+    last_updated_by = serializers.CharField(required=False)
+    lub_timestamp = serializers.DateTimeField(required=False)
+
+
+class TargetHistoryPostSerializer(serializers.Serializer):
+    """
+    Serializes Template History.
+
+    This handles the history of a targets tempaltes.
+    """
+
+    # User Defined
+    email = serializers.EmailField(required=True)
+    history_list = TemplateStatusSerializer(many=True)
+
+
+class TargetHistoryPatchSerializer(serializers.Serializer):
+    """
+    Serializes Template History.
+
+    This handles the history of a targets tempaltes.
+    """
+
+    # User Defined
+    history_list = TemplateStatusSerializer(many=True)
+
+
+class TargetHistoryResponseSerializer(serializers.Serializer):
+    """
+    Serializes the response for a Tag (replaceable token in a template).
+
+    This is a formats the data coming out of the Db.
+    """
+
+    target_uuid = serializers.UUIDField()
