40cb46e0458f64d0abd26f420ea2bdcda3c9b53b
Randy Woods <randy1@radiorandy.com>
2020-07-01 14:12:15 -0600

    Cleanup

--- client/AdminUI/src/app/components/customers/customers.component.ts
+++ client/AdminUI/src/app/components/customers/customers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { LayoutMainService } from 'src/app/services/layout-main.service';
 import { CustomerService } from 'src/app/services/customer.service';
