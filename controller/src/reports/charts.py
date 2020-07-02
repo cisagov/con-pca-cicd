1f628009c23f30e13641f000b472c1dd5ac304e8
inlguy <5023364+inlguy@users.noreply.github.com>
2020-07-02 13:24:21 -0600

    updated need to test with Mostafa's data

--- controller/src/reports/charts.py
+++ controller/src/reports/charts.py
@@ -2,21 +2,20 @@
 import math
 import numpy as np
 
-class ChartGenerator:
-    chartHeight = 275
-
-    def translateBar(self, barHeight, maxvalue):
+class ChartGenerator():
+    chart_height = 275    
+    def translateBar(self, barHeight, maxvalue, first_offSet):
         #moves bar to align it at the bottom axis
         #determine chart height
         #subtract bar height from 100 to get starting point
-        factor = self.chartHeight/maxvalue
+        factor = (self.chart_height-first_offSet)/maxvalue
         return int(factor*barHeight)
      
     def translateGetY(self, barHeight):
         #moves bar to align it at the bottom axis
         #determine chart height
         #subtract bar height from 100 to get starting point    
-        return int(self.chartHeight - barHeight)
+        return int(self.chart_height - barHeight)
 
     def createList(self, r1, r2, step): 
         return np.arange(r1, r2+1, step) 
@@ -58,39 +57,39 @@ class ChartGenerator:
         y_axis = self.createList(0,topRange,tick_range)
         
         y_axisList = " ".join([f"{i};" for i in y_axis])
-        print(y_axisList)
+        #print(y_axisList)
 
-        svg_strg_header_1 = f""""<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="450" height="300" aria-labelledby="title desc" role="img">
+        svg_strg_header_1 = f"""<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="chart" width="450" height="300" aria-labelledby="title desc" role="img">
             <title id="title">STATISTICS BY LEVEL</title>
             <!--this needs to be dynamic--><desc id="units-achieved">{y_axisList}</desc>
-            <g class="measure-line">
-            <line x1="30" y1="5" x2="500" y2="5" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="35" x2="500" y2="35" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="65" x2="500" y2="65" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="95" x2="500" y2="95" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="125" x2="500" y2="125" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="155" x2="500" y2="155" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="185" x2="500" y2="185" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="215" x2="500" y2="215" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="245" x2="500" y2="245" style="stroke:#BAD6E4;stroke-width:2"/>
-            <line x1="30" y1="275" x2="500" y2="275" style="stroke:#BAD6E4;stroke-width:2"/>"""
-            
-        svg_strg_axis_1 = """            
-            <text x="8" y="275" class="caption" dy=".35em">{}</text>
-            <text x="8" y="245" class="caption" dy=".35em">{}</text>
-            <text x="0" y="215" class="caption" dy=".35em">{}</text>
-            <text x="0" y="185" class="caption" dy=".35em">{}</text>
-            <text x="0" y="155" class="caption" dy=".35em">{}</text>
-            <text x="0" y="125" class="caption" dy=".35em">{}</text>
-            <text x="0" y="95" class="caption" dy=".35em">{}</text>
-            <text x="0" y="65" class="caption" dy=".35em">{}</text>
-            <text x="0" y="35" class="caption" dy=".35em">{}</text>
-            <text x="0" y="5" class="caption" dy=".35em">{}</text>
-            </g>""".format(*y_axis)
-        svg_string_3 =f"""<desc id="email-action">{legendStrings}</desc>"""
-        normalized_values = [self.translateBar(x,topRange) for x in values] 
+            <g class="measure-line">"""
+         
+        svg_strg_2 =f"""<desc id="email-action">{legendStrings}</desc>"""
+        dist_between_bars = self.chart_height / len(y_axis)
+        
+        svg_strg_axis_1 = "".join(
+            map(
+                str,
+                [
+                    f"""<line x1="35" y1="{int((i+1) * dist_between_bars)}" x2="500" y2="{int((i+1) * dist_between_bars)}" style="stroke:#BAD6E4;stroke-width:2"/>\n"""                    
+                    for i, x in enumerate(y_axis)
+                ],
+            )
+        )
+        svg_strg_axis_2 = "".join(
+            map(
+                str,
+                [                    
+                    f"""<text x="" y="{int((i+1) * dist_between_bars)}" class="caption" dy=".35em">{x}</text>\n"""
+                    for i, x in enumerate(reversed(y_axis))
+                ],
+            )
+        )
+        
+
+        normalized_values = [self.translateBar(x,topRange,dist_between_bars) for x in values] 
         yvalues = [self.translateGetY(x) for x in normalized_values] 
-        svg_string_4 = f"""<g>
+        svg_string_4 = f"""</g><g>
             <rect style="fill:#164A91;" width="15" height="{normalized_values[0]}" x="60" y="{yvalues[0]}"></rect>
          <rect style="fill:#FDC010;" width="15" height="{normalized_values[1]}" x="80" y="{yvalues[1]}"></rect>
             <rect style="fill:#1979a7" width="15" height="{normalized_values[2]}" x="100" y="{yvalues[2]}"></rect>
@@ -121,5 +120,5 @@ class ChartGenerator:
             <text x="415" y="290" class="caption" dy=".35em">Reported</text>
             </g>
             </svg>"""
-        return svg_strg_header_1 + svg_strg_axis_1 + svg_string_3  + svg_string_4
+        return svg_strg_header_1 + svg_strg_2 + svg_strg_axis_1 + svg_strg_axis_2  + svg_string_4
 
