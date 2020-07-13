d1dd1ef3cd52d4738b884728d2b7aa5db628c51a
Zachary Wilson <wilsonzachary634@gmail.com>
2020-07-13 09:53:31 -0600

    gophish ssl self signed

--- gophish/etc/gophish/entrypoint.sh
+++ gophish/etc/gophish/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+sed -i "s/USE_SELF_SIGNED_CERT/$USE_SELF_SIGNED_CERT/" config.json
+
 sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/" config.json
 sed -i "s/MYSQL_USER/$MYSQL_USER/" config.json
-sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/" config.json
+
+repl=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<<"$MYSQL_PASSWORD")
+sed -i "s/MYSQL_PASSWORD/$repl/" config.json
+
 sed -i "s/MYSQL_HOST/$MYSQL_HOST/" config.json
 
 ./gophish
