44eeabfbf08b252ad1400b637eac66e9835d86c7
pmercado23 <pmercado@ucsc.edu>
2020-07-14 14:12:37 -0400

    fixing serializer issue

--- client/AdminUI/src/app/components/contacts/view-contact-dialog/view-contact-dialog.component.ts
+++ client/AdminUI/src/app/components/contacts/view-contact-dialog/view-contact-dialog.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CustomerService } from 'src/app/services/customer.service';
-import { SubscriptionService } from 'src/app/services/subscription.service';
 import {
   ICustomerContact,
   Customer,
   Contact
 } from 'src/app/models/customer.model';
-import { Subscription } from 'src/app/models/subscription.model';
 
 @Component({
   selector: 'app-view-contact-dialog',
@@ -28,16 +26,13 @@ export class ViewContactDialogComponent implements OnInit {
     notes: new FormControl(),
     active: new FormControl()
   });
-
   customer: Customer;
-  subscription: Subscription;
   initial: ICustomerContact;
   data: ICustomerContact;
 
   constructor(
     public dialog_ref: MatDialogRef<ViewContactDialogComponent>,
     public customer_service: CustomerService,
-    private subscription_service: SubscriptionService,
     @Inject(MAT_DIALOG_DATA) data
   ) {
     this.data = data;
@@ -78,31 +73,9 @@ export class ViewContactDialogComponent implements OnInit {
 
   removeContact(): void {
     const index = this.getContactIndex();
-    console.log('+++start remove contact+++');
-    console.log(this.customer.customer_uuid);
-    console.log(this.customer.contact_list[index]);
-    this.updateSubs(this.customer.contact_list[index]);
-    let blarg = [];
-    if (blarg.length > 0) {
-      // Check if there are any subs with contact, if so, remove them from the sub.
-      console.log(blarg);
-    }
     if (index > -1) {
       this.customer.contact_list.splice(index, 1);
     }
-  }
-
-  updateSubs(primary_contact: Contact) {
-    // Get all subs with customer and primary contact
-    this.subscription_service
-      .getPrimaryContactSubscriptions(
-        this.customer.customer_uuid,
-        primary_contact
-      )
-      .subscribe((subscriptions: Subscription[]) => {
-        console.log(subscriptions);
-        return subscriptions;
-      });
   }
 
   onCancelExitClick(): void {
