128d5ca3c4430c5c9ac27655ddcbac0fc7ab9704
Randy Woods <randy1@radiorandy.com>
2020-07-15 14:00:20 -0600

    Fixed console errors in template.  Styling fixes.

--- client/AdminUI/src/app/components/reports/cycle/cycle.component.ts
+++ client/AdminUI/src/app/components/reports/cycle/cycle.component.ts
@@ -25,8 +25,18 @@ export class CycleComponent implements OnInit {
    *
    */
   ngOnInit(): void {
-    this.reportsSvc.getCycleReport('bf9db4d1-3789-4065-8bab-c663dfecbcfc', new Date()).subscribe(resp => {
+    const u1 = 'bf9db4d1-3789-4065-8bab-c663dfecbcfc';
+    const u2 = 'fd13b4ce-3fb1-4645-961a-175099cb5e83';
+    this.reportsSvc.getCycleReport(u2, new Date()).subscribe(resp => {
       this.detail = resp;
+
+      console.log(resp);
+
+      // RKW - fake in some metrics
+      this.detail.metrics.number_of_clicked_emails = 1;
+      //this.detail.metrics.number_of_email_sent_overall = 100;
+
+
       this.renderReport();
     });
   }
