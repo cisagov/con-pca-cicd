4e40c2fb37a7b35bafddd09767a8900e072da94b
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-07-23 11:30:47 -0500

    remove backend chart generator

--- controller/src/reports/views/cycle_view.py
+++ controller/src/reports/views/cycle_view.py
@@ -21,7 +21,6 @@ from django.views.generic import TemplateView
 
 
 # from . import views
-from reports.charts import ChartGenerator
 from reports.utils import (
     get_subscription_stats_for_cycle,
     get_subscription_stats_for_month,
@@ -46,8 +45,7 @@ logger = logging.getLogger(__name__)
 
 # GoPhish API Manager
 campaign_manager = CampaignManager()
-# Chart Generator
-generate_chart = ChartGenerator()
+
 
 class CycleReportsView(APIView):
     # template_name = "reports/cycle.html"
--- controller/src/reports/views/monthly_view.py
+++ controller/src/reports/views/monthly_view.py
@@ -21,7 +21,6 @@ from django.views.generic import TemplateView
 
 
 # from . import views
-from reports.charts import ChartGenerator
 from reports.utils import (
     get_subscription_stats_for_cycle,
     get_subscription_stats_for_month,
@@ -45,8 +44,6 @@ logger = logging.getLogger(__name__)
 
 # GoPhish API Manager
 campaign_manager = CampaignManager()
-# Chart Generator
-generate_chart = ChartGenerator()
 
 
 class MonthlyReportsView(APIView):
