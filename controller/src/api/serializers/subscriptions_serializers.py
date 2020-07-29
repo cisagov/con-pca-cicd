a56661a38362c7d47fe7c0437795bedfaa0d6afc
mostafa abdo <abdelbaky.mostafa@gmail.com>
2020-07-28 23:31:25 -0500

    add uuid fields to cycle

--- controller/src/api/serializers/subscriptions_serializers.py
+++ controller/src/api/serializers/subscriptions_serializers.py
@@ -141,6 +141,7 @@ class CycleSerializer(serializers.Serializer):
     This is the Cycle serializer used for general reporting.
     """
 
+    cycle_uuid = serializers.UUIDField()
     start_date = serializers.DateTimeField()
     end_date = serializers.DateTimeField()
     active = serializers.BooleanField()
