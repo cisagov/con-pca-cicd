01e7a8b2f08d4b5aab63b360856ae0faf0d583b3
Zachary Wilson <wilsonzachary634@gmail.com>
2020-08-25 09:32:05 -0600

    cleanup gophish

--- gophish/etc/gophish/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/" config.json
-sed -i "s/MYSQL_USER/$MYSQL_USER/" config.json
-repl=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<<"$MYSQL_PASSWORD")
-sed -i "s/MYSQL_PASSWORD/$repl/" config.json
-sed -i "s/MYSQL_HOST/$MYSQL_HOST/" config.json
-
-./gophish
