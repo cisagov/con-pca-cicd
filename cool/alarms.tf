ec9b251077bb68dc9f91f9d4cb679ffaf9cb127a
Zachary Wilson <wilsonzachary634@gmail.com>
2021-01-20 10:24:52 -0700

    Add alarms

--- cool/alarms.tf
+++ cool/alarms.tf
@@ -1,203 +1,180 @@
 # #=========================
 # # API FARGATE
 # #=========================
-# module "api_fargate_alarms" {
-#   source       = "git::https://github.com/cloudposse/terraform-aws-ecs-cloudwatch-sns-alarms.git?ref=master"
-#   namespace    = var.app
-#   stage        = var.env
-#   name         = "api"
-#   cluster_name = aws_ecs_cluster.cluster.name
-#   service_name = aws_ecs_service.api.name
+resource "aws_cloudwatch_metric_alarm" "api_cpu_high" {
+  alarm_name          = "${var.app}-${var.env}-api-cpu-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "Average API CPU Utilization over 80%"
+  alarm_actions       = [aws_sns_topic.notifications.arn, aws_appautoscaling_policy.api_out.arn]
+}
 
-#   cpu_utilization_low_alarm_actions     = [aws_appautoscaling_policy.api_in.arn]
-#   cpu_utilization_high_alarm_actions    = [aws_sns_topic.notifications.arn, aws_appautoscaling_policy.api_out.arn]
-#   cpu_utilization_high_ok_actions       = [aws_sns_topic.notifications.arn]
-#   memory_utilization_high_alarm_actions = [aws_sns_topic.notifications.arn]
-#   memory_utilization_high_ok_actions    = [aws_sns_topic.notifications.arn]
-# }
+resource "aws_cloudwatch_metric_alarm" "api_cpu_low" {
+  alarm_name          = "${var.app}-${var.env}-api-cpu-low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 30
+  alarm_description   = "Average API CPU utilization under 30%"
+  alarm_actions       = [aws_sns_topic.notifications.arn, aws_appautoscaling_policy.api_in.arn]
+}
 
-# #=========================
-# # GOPHISH FARGATE
-# #=========================
-# module "gophish_fargate_alarms" {
-#   source       = "git::https://github.com/cloudposse/terraform-aws-ecs-cloudwatch-sns-alarms.git?ref=master"
-#   namespace    = var.app
-#   stage        = var.env
-#   name         = "gophish"
-#   cluster_name = aws_ecs_cluster.cluster.name
-#   service_name = aws_ecs_service.gophish.name
+#=========================
+# GOPHISH RDS
+#=========================
+resource "aws_cloudwatch_metric_alarm" "gophish_rds_cpu_high" {
+  alarm_name          = "${var.app}-${var.env}-gophish-rds-cpu_high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/RDS"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "Average database CPU utilization over last 10 minutes too high"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+  ok_actions          = ["${aws_sns_topic.notifications.arn}"]
 
-#   cpu_utilization_high_alarm_actions    = [aws_sns_topic.notifications.arn]
-#   cpu_utilization_high_ok_actions       = [aws_sns_topic.notifications.arn]
-#   memory_utilization_high_alarm_actions = [aws_sns_topic.notifications.arn]
-#   memory_utilization_high_ok_actions    = [aws_sns_topic.notifications.arn]
-# }
+  dimensions = {
+    DBInstanceIdentifier = module.rds.instance_id
+  }
+}
 
-# #=========================
-# # BROWSERLESS FARGATE
-# #=========================
-# module "browserless_fargate_alarms" {
-#   source       = "git::https://github.com/cloudposse/terraform-aws-ecs-cloudwatch-sns-alarms.git?ref=master"
-#   namespace    = var.app
-#   stage        = var.env
-#   name         = "browserless"
-#   cluster_name = aws_ecs_cluster.cluster.name
-#   service_name = aws_ecs_service.browserless.name
+resource "aws_cloudwatch_metric_alarm" "gophish_rds_freeable_memory_low" {
+  alarm_name          = "${var.app}-${var.env}-gophish-rds-freeable_memory_too_low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeableMemory"
+  namespace           = "AWS/RDS"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = "64000000" # 64 megabyte in bytes
+  alarm_description   = "Average database freeable memory over last 10 minutes too low, performance may suffer"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+  ok_actions          = ["${aws_sns_topic.notifications.arn}"]
 
-#   cpu_utilization_high_alarm_actions    = [aws_sns_topic.notifications.arn]
-#   cpu_utilization_high_ok_actions       = [aws_sns_topic.notifications.arn]
-#   memory_utilization_high_alarm_actions = [aws_sns_topic.notifications.arn]
-#   memory_utilization_high_ok_actions    = [aws_sns_topic.notifications.arn]
-# }
+  dimensions = {
+    DBInstanceIdentifier = module.rds.instance_id
+  }
+}
 
-# #=========================
-# # GOPHISH RDS
-# #=========================
-# resource "aws_cloudwatch_metric_alarm" "gophish_rds_cpu_high" {
-#   alarm_name          = "${var.app}-${var.env}-gophish-rds-cpu_high"
-#   comparison_operator = "GreaterThanThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "CPUUtilization"
-#   namespace           = "AWS/RDS"
-#   period              = "600"
-#   statistic           = "Average"
-#   threshold           = "80"
-#   alarm_description   = "Average database CPU utilization over last 10 minutes too high"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
-#   ok_actions          = ["${aws_sns_topic.notifications.arn}"]
+resource "aws_cloudwatch_metric_alarm" "gophish_rds_free_storage_space_too_low" {
+  alarm_name          = "${var.app}-${var.env}-gophish-rds-free_storage_space_threshold"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeStorageSpace"
+  namespace           = "AWS/RDS"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = "2000000000" # 2 Gigabyte in bytes
+  alarm_description   = "Average database free storage space over last 10 minutes too low"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+  ok_actions          = ["${aws_sns_topic.notifications.arn}"]
 
-#   dimensions = {
-#     DBInstanceIdentifier = module.rds.instance_id
-#   }
-# }
+  dimensions = {
+    DBInstanceIdentifier = module.rds.instance_id
+  }
+}
 
-# resource "aws_cloudwatch_metric_alarm" "gophish_rds_freeable_memory_low" {
-#   alarm_name          = "${var.app}-${var.env}-gophish-rds-freeable_memory_too_low"
-#   comparison_operator = "LessThanThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "FreeableMemory"
-#   namespace           = "AWS/RDS"
-#   period              = "600"
-#   statistic           = "Average"
-#   threshold           = "64000000" # 64 megabyte in bytes
-#   alarm_description   = "Average database freeable memory over last 10 minutes too low, performance may suffer"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
-#   ok_actions          = ["${aws_sns_topic.notifications.arn}"]
+#=========================
+# Lambda
+#=========================
+resource "aws_cloudwatch_metric_alarm" "queue_tasks_error" {
+  alarm_name          = "${var.app}-${var.env}-queue_tasks-error"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "Queue Tasks Lambda Function Errored"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
 
-#   dimensions = {
-#     DBInstanceIdentifier = module.rds.instance_id
-#   }
-# }
+  dimensions = {
+    FunctionName = aws_lambda_function.queue_tasks.function_name
+  }
+}
 
-# resource "aws_cloudwatch_metric_alarm" "gophish_rds_free_storage_space_too_low" {
-#   alarm_name          = "${var.app}-${var.env}-gophish-rds-free_storage_space_threshold"
-#   comparison_operator = "LessThanThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "FreeStorageSpace"
-#   namespace           = "AWS/RDS"
-#   period              = "600"
-#   statistic           = "Average"
-#   threshold           = "2000000000" # 2 Gigabyte in bytes
-#   alarm_description   = "Average database free storage space over last 10 minutes too low"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
-#   ok_actions          = ["${aws_sns_topic.notifications.arn}"]
+resource "aws_cloudwatch_metric_alarm" "process_tasks_error" {
+  alarm_name          = "${var.app}-${var.env}-process_tasks-error"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "Process Tasks Lambda Function Errored"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
 
-#   dimensions = {
-#     DBInstanceIdentifier = module.rds.instance_id
-#   }
-# }
+  dimensions = {
+    FunctionName = aws_lambda_function.process_tasks.function_name
+  }
+}
 
-# #=========================
-# # DOCUMENT DB
-# #=========================
-# resource "aws_cloudwatch_metric_alarm" "queue_tasks_error" {
-#   alarm_name          = "${var.app}-${var.env}-queue_tasks-error"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "Errors"
-#   namespace           = "AWS/Lambda"
-#   period              = "60"
-#   statistic           = "Sum"
-#   threshold           = "1"
-#   alarm_description   = "Queue Tasks Lambda Function Errored"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+#=========================
+# DOCUMENT DB
+#=========================
+resource "aws_cloudwatch_metric_alarm" "docdb_cpu_high" {
+  alarm_name          = "${var.app}-${var.env}-docdb-cpu_high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/DocDB"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "Average database CPU utilization over last 10 minutes too high"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+  ok_actions          = ["${aws_sns_topic.notifications.arn}"]
 
-#   dimensions = {
-#     FunctionName = aws_lambda_function.queue_tasks.function_name
-#   }
-# }
+  dimensions = {
+    DBClusterIdentifier = module.documentdb.cluster_name
+  }
+}
 
-# resource "aws_cloudwatch_metric_alarm" "process_tasks_error" {
-#   alarm_name          = "${var.app}-${var.env}-process_tasks-error"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "Errors"
-#   namespace           = "AWS/Lambda"
-#   period              = "60"
-#   statistic           = "Sum"
-#   threshold           = "1"
-#   alarm_description   = "Process Tasks Lambda Function Errored"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+resource "aws_cloudwatch_metric_alarm" "docdb_freeable_memory_too_low" {
+  alarm_name          = "${var.app}-${var.env}-docdb-freeable_memory_too_low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeableMemory"
+  namespace           = "AWS/DocDB"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = "64000000" # 64 megabyte in bytes
+  alarm_description   = "Average database freeable memory over last 10 minutes too low, performance may suffer"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+  ok_actions          = ["${aws_sns_topic.notifications.arn}"]
 
-#   dimensions = {
-#     FunctionName = aws_lambda_function.process_tasks.function_name
-#   }
-# }
+  dimensions = {
+    DBClusterIdentifier = module.documentdb.cluster_name
+  }
+}
 
-# #=========================
-# # DOCUMENT DB
-# #=========================
-# resource "aws_cloudwatch_metric_alarm" "docdb_cpu_high" {
-#   alarm_name          = "${var.app}-${var.env}-docdb-cpu_high"
-#   comparison_operator = "GreaterThanThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "CPUUtilization"
-#   namespace           = "AWS/DocDB"
-#   period              = "600"
-#   statistic           = "Average"
-#   threshold           = "80"
-#   alarm_description   = "Average database CPU utilization over last 10 minutes too high"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
-#   ok_actions          = ["${aws_sns_topic.notifications.arn}"]
+resource "aws_cloudwatch_metric_alarm" "docdb_free_local_storage_too_low" {
+  alarm_name          = "${var.app}-${var.env}-docdb-free_local_storage_too_low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "FreeLocalStorage"
+  namespace           = "AWS/DocDB"
+  period              = "600"
+  statistic           = "Average"
+  threshold           = "2000000000" # 2 Gigabyte in bytes
+  alarm_description   = "Average database free storage space over last 10 minutes too low"
+  alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
+  ok_actions          = ["${aws_sns_topic.notifications.arn}"]
 
-#   dimensions = {
-#     DBClusterIdentifier = module.documentdb.cluster_name
-#   }
-# }
-
-# resource "aws_cloudwatch_metric_alarm" "docdb_freeable_memory_too_low" {
-#   alarm_name          = "${var.app}-${var.env}-docdb-freeable_memory_too_low"
-#   comparison_operator = "LessThanThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "FreeableMemory"
-#   namespace           = "AWS/DocDB"
-#   period              = "600"
-#   statistic           = "Average"
-#   threshold           = "64000000" # 64 megabyte in bytes
-#   alarm_description   = "Average database freeable memory over last 10 minutes too low, performance may suffer"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
-#   ok_actions          = ["${aws_sns_topic.notifications.arn}"]
-
-#   dimensions = {
-#     DBClusterIdentifier = module.documentdb.cluster_name
-#   }
-# }
-
-# resource "aws_cloudwatch_metric_alarm" "docdb_free_local_storage_too_low" {
-#   alarm_name          = "${var.app}-${var.env}-docdb-free_local_storage_too_low"
-#   comparison_operator = "LessThanThreshold"
-#   evaluation_periods  = "1"
-#   metric_name         = "FreeLocalStorage"
-#   namespace           = "AWS/DocDB"
-#   period              = "600"
-#   statistic           = "Average"
-#   threshold           = "2000000000" # 2 Gigabyte in bytes
-#   alarm_description   = "Average database free storage space over last 10 minutes too low"
-#   alarm_actions       = ["${aws_sns_topic.notifications.arn}"]
-#   ok_actions          = ["${aws_sns_topic.notifications.arn}"]
-
-#   dimensions = {
-#     DBClusterIdentifier = module.documentdb.cluster_name
-#   }
-# }
+  dimensions = {
+    DBClusterIdentifier = module.documentdb.cluster_name
+  }
+}
 
