7885203a67206ffce7ccd9be0284811b3ccbfae5
Zachary Wilson <wilsonzachary634@gmail.com>
2021-03-02 06:50:31 -0700

    change role for accessing shared services backend

--- cool/remote_states.tf
+++ cool/remote_states.tf
@@ -23,7 +23,7 @@ data "terraform_remote_state" "sharedservices_networking" {
     dynamodb_table = "terraform-state-lock"
     region         = "us-east-1"
     key            = "cool-sharedservices-networking/terraform.tfstate"
-    role_arn       = "arn:aws:iam::210193616405:role/AccessPCATerraformBackend"
+    role_arn       = "arn:aws:iam::210193616405:role/ReadSharedServicesNetworkingTerraformState"
     session_name   = "pca-github-actions"
   }
 
