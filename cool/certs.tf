ecf4c7fe264b5d7c0fb9a3c8ef21a976a20bee91
Zachary Wilson <zachary.wilson@inl.gov>
2021-04-27 11:44:48 -0600

    version cert modules

--- cool/certs.tf
+++ cool/certs.tf
@@ -2,25 +2,29 @@
 # Certs
 # ===========================
 module "web_certs" {
-  source      = "github.com/terraform-aws-modules/terraform-aws-acm"
+  source      = "terraform-aws-modules/acm/aws"
+  version     = "~> v2.14.0"
   domain_name = aws_route53_record.sharedservices_internal_web.name
   zone_id     = aws_route53_zone.public_zone.zone_id
 }
 
 module "gophish_certs" {
-  source      = "github.com/terraform-aws-modules/terraform-aws-acm"
+  source      = "terraform-aws-modules/acm/aws"
+  version     = "~> v2.14.0"
   domain_name = aws_route53_record.sharedservices_internal_gophish.name
   zone_id     = aws_route53_zone.public_zone.zone_id
 }
 
 module "api_certs" {
-  source      = "github.com/terraform-aws-modules/terraform-aws-acm"
+  source      = "terraform-aws-modules/acm/aws"
+  version     = "~> v2.14.0"
   domain_name = aws_route53_record.sharedservices_internal_api.name
   zone_id     = aws_route53_zone.public_zone.zone_id
 }
 
 module "public_certs" {
-  source      = "github.com/terraform-aws-modules/terraform-aws-acm"
+  source      = "terraform-aws-modules/acm/aws"
+  version     = "~> v2.14.0"
   domain_name = aws_route53_record.public.name
   zone_id     = aws_route53_zone.public_zone.zone_id
 }
