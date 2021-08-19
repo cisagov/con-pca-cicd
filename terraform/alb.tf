0d242607810b83c8ea04436f3d56b691694d61c8
Zachary Wilson <zachary.wilson@inl.gov>
2021-08-19 08:57:29 -0600

    fix default action

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -41,10 +41,12 @@ resource "aws_lb_listener" "https" {
   protocol          = "HTTPS"
   certificate_arn   = module.acm.this_acm_certificate_arn
 
-  fixed_response {
-    content_type = "text/plain"
-    message_body = "${var.app}-${var.env} fixed response"
-    status_code  = 200
+  default_action {
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "${var.app}-${var.env} fixed response"
+      status_code  = 200
+    }
   }
 }
 
@@ -52,10 +54,11 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = module.alb.alb_arn
   port              = 80
   protocol          = "HTTP"
-
-  fixed_response {
-    content_type = "text/plain"
-    message_body = "${var.app}-${var.env} fixed response"
-    status_code  = 200
+  default_action {
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "${var.app}-${var.env} fixed response"
+      status_code  = 200
+    }
   }
 }
