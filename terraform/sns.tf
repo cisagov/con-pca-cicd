51bc1d86d06abceeb3729f4229e2d9feb7c23792
Zachary Wilson <zachary.wilson@inl.gov>
2021-11-11 07:19:50 -0700

    Events for ecs tasks stopped

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,3 +1,22 @@
 resource "aws_sns_topic" "notifications" {
   name = "${var.app}-${var.env}-notifications"
 }
+
+data "aws_iam_policy_document" "notifications" {
+  statement {
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = [aws_sns_topic.notifications.arn]
+  }
+}
+
+resource "aws_sns_topic_policy" "notifications" {
+  arn    = aws_sns_topic.notifications.arn
+  policy = data.aws_iam_policy_document.notifications.json
+}
