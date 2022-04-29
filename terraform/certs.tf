5e6bcea94aeefe7ddc192a74710a6d5f5d5c6c48
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-04-28 17:37:21 -0700

    try different acm/aws version

--- terraform/certs.tf
+++ terraform/certs.tf
@@ -3,7 +3,7 @@
 # ===========================
 module "acm" {
   source      = "terraform-aws-modules/acm/aws"
-  version     = "3.4.1"
+  version     = "3.4.0"
   domain_name = aws_route53_record.domain.name
   zone_id     = data.aws_route53_zone.zone.zone_id
 }
