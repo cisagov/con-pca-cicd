4315190fb59c01f1502296938bf8b3d92bf6f05a
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-04-28 17:45:11 -0700

    update terraform version and acm/aws version

--- cool/certs.tf
+++ cool/certs.tf
@@ -1,7 +1,7 @@
 # Certs for internal load balancer
 module "internal_certs" {
   source      = "terraform-aws-modules/acm/aws"
-  version     = "3.4.0"
+  version     = "3.4.1"
   domain_name = aws_route53_record.sharedservices_internal_web.name
   zone_id     = aws_route53_zone.public_zone.zone_id
 }
@@ -9,7 +9,7 @@ module "internal_certs" {
 # Certs for public load balancer
 module "public_certs" {
   source      = "terraform-aws-modules/acm/aws"
-  version     = "3.4.0"
+  version     = "3.4.1"
   domain_name = aws_route53_record.public.name
   zone_id     = aws_route53_zone.public_zone.zone_id
 }
