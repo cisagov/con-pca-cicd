bd65f6ab81d95fe72156dd62ca981fa5ffaebc98
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-09-16 18:11:33 -0700

    remove public certs

--- cool/certs.tf
+++ cool/certs.tf
@@ -6,10 +6,10 @@ module "internal_certs" {
   zone_id     = aws_route53_zone.public_zone.zone_id
 }
 
-# Certs for public load balancer
-module "public_certs" {
-  source      = "terraform-aws-modules/acm/aws"
-  version     = "3.4.1"
-  domain_name = aws_route53_record.public.name
-  zone_id     = aws_route53_zone.public_zone.zone_id
-}
+# # Certs for public load balancer
+# module "public_certs" {
+#   source      = "terraform-aws-modules/acm/aws"
+#   version     = "3.4.1"
+#   domain_name = aws_route53_record.public.name
+#   zone_id     = aws_route53_zone.public_zone.zone_id
+# }
