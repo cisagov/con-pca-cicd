c273718f3e26512242ee71b0ff0076112468f497
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2022-10-20 15:04:14 -0700

    update: env vars to be pulled from gh actions

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,27 +9,12 @@ locals {
   api_container_protocol = "HTTP"
 
   api_environment = {
-    "FLASK_APP" : "main"
-    "FLASK_DEBUG" : 0
-    "DB_PW" : aws_ssm_parameter.docdb_password.value
-    "DB_USER" : aws_ssm_parameter.docdb_username.value
-    "DB_HOST" : module.documentdb.endpoint
-    "DB_PORT" : 27017
-    "MONGO_TYPE" : "DOCUMENTDB"
-    "WORKERS" : 6
-    "AWS_COGNITO_ENABLED" : 1
-    "AWS_COGNITO_USER_POOL_ID" : aws_cognito_user_pool.pool.id
-    "AWS_COGNITO_USER_POOL_CLIENT_ID" : aws_cognito_user_pool_client.client.id
-    "SES_ASSUME_ROLE_ARN" : var.ses_arn
-    "SMTP_FROM" : "pca-sandbox@cyber.dhs.gov"
     "DEPLOYED_DATE" : var.deployed_date
     "API_COMMIT_ID" : var.api_image_tag
     "UI_COMMIT_ID" : var.ui_image_tag
     "AWS_REGION" : var.region
     "AWS_DEFAULT_REGION" : var.region
     "LANDING_SUBDOMAIN" : var.landing_subdomain
-    "MAXMIND_USER_ID" : var.maxmind_user_id
-    "MAXMIND_LICENSE_KEY" : var.maxmind_license_key
   }
 
   # Landing Locals
