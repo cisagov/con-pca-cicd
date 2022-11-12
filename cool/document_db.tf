39f3dd5e5f6e561a089db093d256b7c81131f0f0
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-11-12 13:53:00 -0800

    add docdb4.0 to COOL env

--- cool/document_db.tf
+++ cool/document_db.tf
@@ -32,3 +32,23 @@ module "documentdb" {
   subnet_ids              = local.private_subnet_ids
   vpc_id                  = local.vpc_id
 }
+
+module "docdb" {
+  source  = "cloudposse/documentdb-cluster/aws"
+  version = "0.13.0"
+
+  namespace = var.app
+  stage     = var.env
+  name      = "db"
+
+  allowed_security_groups = [aws_security_group.service.id]
+  cluster_family          = "docdb4.0"
+  cluster_size            = 1
+  instance_class          = var.documentdb_instance_class
+  master_username         = random_string.docdb_username.result
+  master_password         = random_password.docdb_password.result
+  skip_final_snapshot     = true
+  snapshot_identifier     = var.documentdb_snapshot_identifier
+  subnet_ids              = local.private_subnet_ids
+  vpc_id                  = local.vpc_id
+}
