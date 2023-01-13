f7be20fc02d4db8119d2472ea09d0c2db366b546
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-01-13 13:45:50 -0800

    update atlas proj resource name

--- sandbox/mongo.tf
+++ sandbox/mongo.tf
@@ -63,7 +63,7 @@ resource "mongodbatlas_database_user" "db-user" {
   username           = random_string.mongodb_username.result
   password           = random_password.mongodb_password.result
   auth_database_name = "admin"
-  project_id         = mongodbatlas_project.aws_atlas.id
+  project_id         = mongodbatlas_project.con-pca.id
   roles {
     role_name     = "readWrite"
     database_name = "admin"
