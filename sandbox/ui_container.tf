a706fe64b5bfaab3bf785d15e589113c0621fc5c
Mostafa Abdo <abdelbaky.mostafa@gmail.com>
2023-01-23 20:03:22 -0800

    add environment to ui env var

--- sandbox/ui_container.tf
+++ sandbox/ui_container.tf
@@ -32,6 +32,7 @@ module "ui_container" {
 
   map_environment = {
     API_URL       = "https://${aws_route53_record.domain.name}"
+    ENVIRONMENT   = var.env
     DEPLOYED_DATE = var.deployed_date
     UI_COMMIT_ID  = var.ui_image_tag
   }
